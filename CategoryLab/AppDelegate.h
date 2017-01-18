4ba6d02659e2c8e7c8f447fdd5935291f3dbf12f
Evan Beh <imexlight@gmail.com>
2017-01-18 11:27:42 +0800

    Initial Commit

---
+++ CategoryLab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CategoryLab
+//
+//  Created by Evan Beh on 18/01/2017.
+//  Copyright © 2017 Evan Beh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CategoryLab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CategoryLab
+//
+//  Created by Evan Beh on 18/01/2017.
+//  Copyright © 2017 Evan Beh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
